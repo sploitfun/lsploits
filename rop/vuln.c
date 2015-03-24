a597d099ff714af0a843c2efebbd18c7ed5f702b
sploitfun <sploitfun@gmail.com>
2015-03-24 20:11:14 +0530

    Shared with Reno

---
+++ rop/vuln.c
@@ -0,0 +1,20 @@
+// vuln.c
+// gcc -o vuln vuln.c -fno-stack-protector -fno-pie -mpreferred-stack-boundary=2
+#include <string.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+	char buf[256];
+	int i;
+ 	seteuid (getuid());
+	if (argc < 2)
+	{
+ 		puts ("Need an argument\n");
+ 		exit (1);
+	}
+	// vulnerable code
+ 	strcpy (buf, argv[1]);
+	printf ("%s\nLen:%d\n", buf, (int)strlen(buf));
+	return (0);
+}
